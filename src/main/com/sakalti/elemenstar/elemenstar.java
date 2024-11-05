35c55ca843029f21138f834f8b3f0874c456959b
Ingushe <167761978+Sakalt@users.noreply.github.com>
2024-11-05 19:22:30 +0900

    Create elemenstar.java

---
+++ src/main/com/sakalti/elemenstar/elemenstar.java
@@ -0,0 +1,12 @@
+package com.sakalti.elemenstars
+
+import net.minecraftforge.fml.common.Mod;
+
+@mod("elemenstars")
+public class elemenstars {
+  
+  public static final String MOD_ID ="elemenstars"; 
+
+  public elemenstars(){
+  }
+}
