f39e4e0fa8797855c297ca5b6d0ca19288e99118
Ingushe <167761978+Sakalt@users.noreply.github.com>
2024-11-05 19:30:09 +0900

    Create firelandstab.java

---
+++ src/main/com/sakalti/elemenstars/tabs/firelandstab.java
@@ -0,0 +1,14 @@
+package com.sakalti.elemenstars;
+
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+
+public class firelandstab {
+
+  public static final Item[] items = {
+    
+    Items.NETHERRACK,
+    Items.STONE
+  };
+}
+    
