adf00ba2d18be4fce9d8eb36aa0355c77f2365b7
Ingushe <167761978+Sakalt@users.noreply.github.com>
2024-11-06 07:33:29 +0900

    Update firelandstab.java

--- src/main/com/sakalti/elemenstars/tabs/firelandstab.java
+++ src/main/com/sakalti/elemenstars/tabs/firelandstab.java
@@ -1,11 +1,12 @@
 package com.sakalti.elemenstars.tabs;
 
 import com.sakalti.elemenstars.items.FireSword;
-import com.sakalti.elemenstars.items.FireGreatSword; // FireGreatSwordをインポート
+import com.sakalti.elemenstars.items.FireGreatSword;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Items;
 import net.minecraftforge.event.CreativeModeTabEvent;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
@@ -17,34 +18,23 @@ import com.sakalti.elemenstars.elemenstars; // MODのメインクラスをイン
 public class FirelandTab {
 
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, elemenstars.MOD_ID);
-
-    public static final RegistryObject<Item> NETHERRACK_ITEM = ITEMS.register("netherrack_item", () -> new Item(new Item.Properties()));
-    public static final RegistryObject<Item> STONE_ITEM = ITEMS.register("stone_item", () -> new Item(new Item.Properties()));
+    
     public static final RegistryObject<Item> FIRE_SWORD = ITEMS.register("fire_sword", FireSword::new);
-    public static final RegistryObject<Item> FIRE_GREAT_SWORD = ITEMS.register("fire_great_sword", FireGreatSword::new); // FireGreatSwordを追加
-
-    public static final Item[] items = {
-        NETHERRACK_ITEM.get(),
-        STONE_ITEM.get(),
-        FIRE_SWORD.get(),
-        FIRE_GREAT_SWORD.get() // FireGreatSwordを追加
+    public static final RegistryObject<Item> FIRE_GREAT_SWORD = ITEMS.register("fire_great_sword", FireGreatSword::new);
+    
+    // 新しいタブの登録
+    public static final CreativeModeTab FIRELAND_TAB = new CreativeModeTab("fireland_tab") {
+        @Override
+        public ItemStack makeIcon() {
+            return new ItemStack(FIRE_SWORD.get());
+        }
     };
 
     @SubscribeEvent
-    public static void onRegisterItems(RegistryEvent.Register<Item> event) {
-        for (Item item : items) {
-            event.getRegistry().register(item);
-        }
-    }
-
-    @SubscribeEvent
     public static void onCreativeTabBuild(CreativeModeTabEvent.BuildContents event) {
-        if (event.getTab() == CreativeModeTabs.BUILDING_BLOCKS) {
-            event.accept(NETHERRACK_ITEM);
-            event.accept(STONE_ITEM);
-        } else if (event.getTab() == CreativeModeTabs.COMBAT) {
+        if (event.getTab() == FIRELAND_TAB) {
             event.accept(FIRE_SWORD);
-            event.accept(FIRE_GREAT_SWORD); // FireGreatSwordを追加
+            event.accept(FIRE_GREAT_SWORD);
         }
     }
 }
