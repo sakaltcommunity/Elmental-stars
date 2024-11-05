6fa16dfa753695042598c0bc791162064a5477a6
Ingushe <167761978+Sakalt@users.noreply.github.com>
2024-11-05 19:50:06 +0900

    Update firelandstab.java

--- src/main/com/sakalti/elemenstars/tabs/firelandstab.java
+++ src/main/com/sakalti/elemenstars/tabs/firelandstab.java
@@ -1,7 +1,7 @@
 package com.sakalti.elemenstars.tabs;
 
+import com.sakalti.elemenstars.items.FireSword;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Item.Properties;
 import net.minecraft.world.item.Items;
 import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.event.RegistryEvent;
@@ -10,19 +10,21 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
-import com.sakalti.elemenstars.ElemenStars; // MODのメインクラスをインポート
+import com.sakalti.elemenstars.elemenstars; // MODのメインクラスをインポート
 
-@Mod.EventBusSubscriber(modid = ElemenStars.MOD_ID)
+@Mod.EventBusSubscriber(modid = elemenstars.MOD_ID)
 public class FirelandTab {
 
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ElemenStars.MOD_ID);
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, elemenstars.MOD_ID);
 
-    public static final RegistryObject<Item> NETHERRACK_ITEM = ITEMS.register("netherrack_item", () -> new Item(new Properties()));
-    public static final RegistryObject<Item> STONE_ITEM = ITEMS.register("stone_item", () -> new Item(new Properties()));
+    public static final RegistryObject<Item> NETHERRACK_ITEM = ITEMS.register("netherrack_item", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> STONE_ITEM = ITEMS.register("stone_item", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> FIRE_SWORD = ITEMS.register("fire_sword", FireSword::new);
 
     public static final Item[] items = {
         NETHERRACK_ITEM.get(),
-        STONE_ITEM.get()
+        STONE_ITEM.get(),
+        FIRE_SWORD.get()
     };
 
     @SubscribeEvent
@@ -37,6 +39,8 @@ public class FirelandTab {
         if (event.getTab() == CreativeModeTabs.BUILDING_BLOCKS) {
             event.accept(NETHERRACK_ITEM);
             event.accept(STONE_ITEM);
+        } else if (event.getTab() == CreativeModeTabs.COMBAT) {
+            event.accept(FIRE_SWORD);
         }
     }
 }
