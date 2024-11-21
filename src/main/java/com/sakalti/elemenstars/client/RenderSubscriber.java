6c1f7eec7d4e937fbd31d2ff04f432cf5f07ac6f
Ingushe <167761978+Sakalt@users.noreply.github.com>
2024-11-21 13:44:42 +0900

    Update RenderSubscriber.java

--- src/main/java/com/sakalti/elemenstars/client/RenderSubscriber.java
+++ src/main/java/com/sakalti/elemenstars/client/RenderSubscriber.java
@@ -1,25 +1,23 @@
 package com.sakalti.elemenstars.client;
 
-import com.sakalti.elemenstars.entities.ZalgEntity;
+import com.sakalti.elemenstars.client.renderer.IceSlimeRenderer;
 import com.sakalti.elemenstars.client.renderer.ZalgRenderer;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import com.sakalti.elemenstars.entities.IceSlimeEntity;
+import com.sakalti.elemenstars.entities.ZalgEntity;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.ModEventSubscriber;
+import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
-@ModEventSubscriber(modid = "elemenstars", bus = ModEventSubscriber.Bus.MOD, value = Dist.CLIENT)
+@Mod.EventBusSubscriber(modid = "elemenstars", bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class RenderSubscriber {
 
-    // エンティティレンダラーの登録
-    @OnlyIn(Dist.CLIENT)
     @SubscribeEvent
-    public static void registerEntityRenderers(IEventBus eventBus) {
-        // Zalgエンティティのレンダラー登録
-        eventBus.addListener((event) -> {
-            Minecraft.getInstance().getEntityRenderDispatcher().renderers.put(ZalgEntity.ZALG.get(), new ZalgRenderer(Minecraft.getInstance().getEntityRenderDispatcher()));
-        });
+    public static void registerEntityRenderers(EntityRenderersEvent.RegisterRenderers event) {
+        // IceSlime のレンダラー登録
+        event.registerEntityRenderer(IceSlimeEntity.ICE_SLIME, IceSlimeRenderer::new);
+
+        // Zalg のレンダラー登録（既存の ZalgRenderer を使用）
+        event.registerEntityRenderer(ZalgEntity.ZALG, ZalgRenderer::new);
     }
 }
